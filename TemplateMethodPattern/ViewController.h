422f2a2e22b4bbbad20f5511e9ffc8997d949f19
liangjie <liangjiestrong@163.com>
2017-03-16 14:29:42 +0800

    Initial Commit

---
+++ TemplateMethodPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TemplateMethodPattern
+//
+//  Created by liangjie on 2017/3/16.
+//  Copyright © 2017年 liangjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
